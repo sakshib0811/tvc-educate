7e8b5aeaee557106df0f7d70b43b7db8ea5b9c59
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-10-11 00:18:01 +0530

    landing Page made responsive & bug fix in carousel

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./styles/main.css";
 import AppProviders from "./components/AppProviders/AppProviders";
 import MainRouter from "./MainRouter";
+import useMediaQuery from "./hooks/useMediaQuery";
+
+
+export const UtilityContext = React.createContext({ isMobile: false });
 
 const App = () => {
+
+  const isMobile = useMediaQuery('(max-width: 580px)');
+
+  const isTablet=useMediaQuery('(max-width: 800px')
+
   return (
-    <AppProviders>
-      <MainRouter />
-    </AppProviders>
+    <UtilityContext.Provider value={{ isMobile }}>
+      <AppProviders>
+        <MainRouter />
+      </AppProviders>
+    </UtilityContext.Provider>
   );
 };
 
